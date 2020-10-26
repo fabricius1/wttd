5d52eaf5a5695027a136cb0c38a3252323f2f268
fabricius1 <fabriciusbr@gmail.com>
2020-10-26 08:54:58 -0300

    Add tests to home page

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+# o TestCase é uma classe do Django que, por sua vez
+# já estende o TestCase do unittest
+class HomeTest(TestCase):
+    def setUp(self):
+        "Access the route '/' with GET method and save it in a class response attribute"
+        self.response = self.client.get('/')        
+
+    def test_get(self):
+        """GET / must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+    
+    def test_template(self):
+        """Must use index.html"""
+        self.assertTemplateUsed(self.response, 'index.html')
