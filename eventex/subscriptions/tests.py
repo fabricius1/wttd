4e488848d7a1fac022fcbe50dde011a9b3a77cbf
fabricius1 <fabriciusbr@gmail.com>
2020-10-29 09:17:01 -0300

    Subscriptions logic implemented

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core import mail
 from eventex.subscriptions.forms import SubscriptionForm
 
 
@@ -40,3 +41,71 @@ class SubscribeTest(TestCase):
         """Form must have four fields"""
         form = self.response.context["form"]
         self.assertSequenceEqual(['name', 'cpf', 'email', 'phone'], list(form.fields))
+
+class SubscribePostTest(TestCase):
+    def setUp(self):
+        data = dict(name='Henrique Bastos', 
+                    cpf='12345678901', 
+                    email="h@gmail.com",
+                    phone="(61)96161-6161")
+        self.response = self.client.post('/inscricao/', data)
+
+
+    def test_post(self):
+        """Valid POST should redirect to /inscricao/"""
+        # 302 is the status code for redirect to another page
+        self.assertEqual(302, self.response.status_code)
+
+    def test_send_subscribe_email(self):
+        self.assertEqual(1, len(mail.outbox))
+
+    def test_subscription_email_subject(self):
+        email = mail.outbox[0]
+        expect = 'Confirmação de inscrição'
+        self.assertEqual(expect, email.subject)
+    
+    def test_subscription_email_from(self):
+        email = mail.outbox[0]
+        expect = 'contato@eventex.com.br'
+        self.assertEqual(expect, email.from_email)
+
+    def test_subscription_email_to(self):
+        email = mail.outbox[0]
+        expect = ['contato@eventex.com.br', 'h@gmail.com']
+        self.assertEqual(expect, email.to)
+
+    def test_subscription_email_body(self):
+        email = mail.outbox[0]
+        self.assertIn('Henrique Bastos', email.body)
+        self.assertIn('12345678901', email.body)
+        self.assertIn('h@gmail.com', email.body)
+        self.assertIn('(61)96161-6161', email.body)
+
+class SubscribeInvalidPost(TestCase):
+    def setUp(self):
+        self.response = self.client.post('/inscricao/', {})
+
+    def test_post(self):
+        """Invalid POST should not redirect"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'subscriptions/subscription_form.html')
+
+    def test_has_form(self):
+        form = self.response.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+    
+    def test_form_has_errors(self):
+        form = self.response.context['form']
+        self.assertTrue(form.errors)
+    
+class SubscribeSuccessMessage(TestCase):
+    def test_message(self):
+        data = dict(name='Henrique Bastos',
+                    cpf='12345678901',
+                    email='henrique@bastos.net',
+                    phone='21-99618-6180')
+        response = self.client.post('/inscricao/', data, follow=True)
+        self.assertContains(response, 'Inscrição realizada com sucesso!')
+
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,7 +1,30 @@
+from django.contrib import messages
 from django.shortcuts import render
 from eventex.subscriptions.forms import SubscriptionForm
+from django.template.loader import render_to_string
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core import mail
+
 
 def subscribe(request):
-    context = {'form': SubscriptionForm()}
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+        # O método abaixo faz a validação dos dados do formulário
+        if form.is_valid():
+            # o contexto agora será o resultado da limpeza do formulário
+            body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
+            mail.send_mail('Confirmação de inscrição',
+                            body,
+                            'contato@eventex.com.br',     # remetente
+                            ['contato@eventex.com.br',
+                            form.cleaned_data['email']]) # lista destinatários
 
-    return render(request, 'subscriptions/subscription_form.html', context)
+            messages.success(request, 'Inscrição realizada com sucesso!')
+            
+            return HttpResponseRedirect('/inscricao/')
+        # Aqui será para quando houver um erro de validação no formulário.
+        else:
+            return render(request, 'subscriptions/subscription_form.html', {'form': form })
+    else:
+        context = {'form': SubscriptionForm()}
+        return render(request, 'subscriptions/subscription_form.html', context)
