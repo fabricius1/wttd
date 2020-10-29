9817750ab1ebf1c287bb1ae8d6c11bb966c1cb26
fabricius1 <fabriciusbr@gmail.com>
2020-10-29 16:50:06 -0300

    Gmail SMTP service configured

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -15,8 +15,8 @@ def subscribe(request):
             body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
             mail.send_mail('Confirmação de inscrição',
                             body,
-                            'contato@eventex.com.br',     # remetente
-                            ['contato@eventex.com.br',
+                            'fabriciobrasilemail@gmail.com',     # remetente
+                            ['fabriciobrasilemail@gmail.com',
                             form.cleaned_data['email']]) # lista destinatários
 
             messages.success(request, 'Inscrição realizada com sucesso!')
