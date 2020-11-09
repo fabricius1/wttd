dce781d7e4203f95b9d707bfea7ccd8dca52624d
fabricius1 <fabriciusbr@gmail.com>
2020-11-09 11:02:08 -0300

    Contact model created

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import options
 from django.shortcuts import resolve_url as r
 
 
@@ -18,3 +19,24 @@ class Speaker(models.Model):
 
     def get_absolute_url(self):
         return r('speaker_detail', slug=self.slug)
+
+
+class Contact(models.Model):
+    EMAIL = 'E'
+    PHONE = 'P'
+    KINDS = (
+        (EMAIL, 'Email'),
+        (PHONE, 'Phone'),
+    )
+
+    speaker = models.ForeignKey(
+        'Speaker', on_delete=models.CASCADE, verbose_name='speaker')
+    kind = models.CharField(max_length=1, choices=KINDS)
+    value = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'contact'
+        verbose_name_plural = 'contacts'
+
+    def __str__(self):
+        return self.value
